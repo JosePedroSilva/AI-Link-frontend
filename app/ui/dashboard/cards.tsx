0af6f0550b0a58db332807d1f5d75642ad437947
Jose Pedro Silva <josepedro88@live.com.pt>
2024-03-10 18:47:31 +0100

    Refactor dashboard cards component

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -26,8 +26,7 @@ import {
     return (
       <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
         <div className="flex p-4">
-          <InboxIcon className="h-5 w-5 text-gray-700" />
-          {/* {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null} */}
+          {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
           <h3 className="ml-2 text-sm font-medium">{title}</h3>
         </div>
         <p
